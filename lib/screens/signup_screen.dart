8114b4080cdb55f754665499bef7c252c71e9e7f
o_omki <omkarmurty7@gmail.com>
2023-05-01 08:28:20 +0530

    Sign-Up screen done.

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,5 @@
+import "package:doctor_appointment/screens/login_screen.dart";
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 
 class SignUpScreen extends StatefulWidget {
@@ -8,15 +10,150 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  bool passToggle = true;
+
   @override
   Widget build(BuildContext context) {
-    bool passToggle = true;  
 
-    return Material(
-      color: Colors.white,
-      child: SingleChildScrollView(
-        child: SafeArea(
-          child: Column,
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Material(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Image.asset("images/doctor5.jpg"),
+                ),
+                const SizedBox(height: 15),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Full Name",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Phone Number",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.phone),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Email Address",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                ),
+                
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextField(
+                    obscureText: passToggle,
+                    decoration: InputDecoration(
+                      label: const Text("Email Password"),
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: passToggle
+                            ? const Icon(CupertinoIcons.eye_slash_fill)
+                            : const Icon(CupertinoIcons.eye_fill),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Material(
+                      color: const Color(0xFF7165D6),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {
+                          // Navigator.push(context, MaterialPageRoute(
+                          //   builder: (context) {
+                          //     return const LoginScreen();
+                          //   },
+                          // ),);
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 40,
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Create Account",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push( 
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const LoginScreen();
+                            },
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF7165D6),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
